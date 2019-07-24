439ef7e7ac78de302fda0fa45d8f447dbfc6089e
Chad Smith <smith.chad12@gmail.com>
2019-07-24 00:03:16 -0500

    add solution to that counts vowels in a strong

---
+++ csharp/DailyChallenges.Tests/VowelCounterTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DailyChallengesLib.VowelCounter;
+using Xunit;
+
+namespace DailyChallenges.Tests
+{
+    public class VowelCounterTests
+    {
+        private readonly VowelCounter _vowelCounter;
+
+        public VowelCounterTests()
+        {
+            _vowelCounter = new VowelCounter();
+        }
+
+        [Fact]
+        public void CountsVowelsInLowerCaseString_Test()
+        {
+            // Arrange
+            var test = "aeiou";
+            var expected = 5;
+
+            // Act
+            var result = _vowelCounter.CountVowels(test);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CountsVowelsInUperCaseString_Test()
+        {
+            // Arrange
+            var test = "AEIOU";
+            var expected = 5;
+
+            // Act
+            var result = _vowelCounter.CountVowels(test);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CountsVowelsWithMixedString_Test()
+        {
+            // Arrange
+            var test = "this is a test";
+            var expected = 4;
+
+            // Act
+            var result = _vowelCounter.CountVowels(test);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ csharp/DailyChallengesLib/VowelCounter/VowelCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyChallengesLib.VowelCounter
+{
+    public class VowelCounter
+    {
+        /// <summary>
+        /// Counts the number of vowels that are in a string.
+        /// A vowel is defined as: "a, e, i, o, u" upper and lowercase.
+        /// </summary>
+        /// <param name="value">The string value to count the number of vowels in.</param>
+        /// <returns>The number of vowels that were in the string.</returns>
+        public int CountVowels(string value)
+        {
+            var vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            return value.Count(x => vowels.Contains(x));
+        }
+    }
+}
