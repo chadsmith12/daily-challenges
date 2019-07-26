eddcbee33e35be34bf3526a6fc6b56b997b322c9
Chad Smith <smith.chad12@gmail.com>
2019-07-26 00:14:08 -0500

    add solution to check if number is in the fibonacci sequene

---
+++ csharp/DailyChallenges.Tests/FibonacciTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using DailyChallengesLib.Fibonacci;
+
+namespace DailyChallenges.Tests
+{
+    public class FibonacciTests
+    {
+        [Fact]
+        public void NumberIsInFibonacciSequence_Test()
+        {
+            var testCase = 5;
+            var expected = true;
+
+            var actual = Fibonacci.IsInFibonacciSequence(testCase);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NumberIsNotInFibonacciSequence_Test()
+        {
+            var testCase = 7;
+            var expected = false;
+
+            var actual = Fibonacci.IsInFibonacciSequence(testCase);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ csharp/DailyChallengesLib/Fibonacci/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyChallengesLib.Fibonacci
+{
+    public static class Fibonacci
+    {
+        public static bool IsInFibonacciSequence(int searchValue, int number1 = 0, int number2 = 1)
+        {
+            var nextSequence = number1 + number2;
+
+            if(nextSequence == searchValue)
+            {
+                return true;
+            }
+            if(nextSequence > searchValue)
+            {
+                return false;
+            }
+
+            return IsInFibonacciSequence(searchValue, number2, nextSequence);
+        }
+    }
+}
