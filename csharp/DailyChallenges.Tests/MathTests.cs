b1d8bd3d27105d6aa9b8a0db41c210fe673304ff
Chad Smith <smith.chad12@gmail.com>
2019-07-31 00:29:12 -0500

    add ability to add and subtract fractions

--- csharp/DailyChallenges.Tests/MathTests.cs
+++ csharp/DailyChallenges.Tests/MathTests.cs
@@ -40,6 +40,131 @@ namespace DailyChallenges.Tests
             Assert.True(result);
         }
 
+        [Fact]
+        public void CreatesNewFractionSetsNumerator_Test()
+        {
+            // Arrange
+            var numerator = 5;
+            var denominator = 10;
+
+            // Act
+            var fraction = new Fraction(numerator, denominator);
+
+            // Assert
+            Assert.Equal(numerator, fraction.Numerator);
+        }
+
+        [Fact]
+        public void CreatesNewFractionSetsDenominator_Test()
+        {
+            // Arrange
+            var numerator = 5;
+            var denominator = 10;
+
+            // Act
+            var fraction = new Fraction(numerator, denominator);
+
+            // Assert
+            Assert.Equal(denominator, fraction.Denominator);
+        }
+
+        [Fact]
+        public void ThrowsArgumentExceptionOnZeroDenominator()
+        {
+            // Arrange
+            var numerator = 5;
+            var denominator = 0;
+            var expectedMessage = "Denominator can not be zero";
+
+            // Act
+            var exception = Assert.Throws<ArgumentException>(() => new Fraction(numerator, denominator));
+
+            // Assert
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void AddsFractionsWithCommonDenominators_Test()
+        {
+            // Arrange
+            var lhs = new Fraction(5, 10);
+            var rhs = new Fraction(2, 10);
+            var expectedNumerator = 7;
+
+            // Act
+            var result = lhs + rhs;
+
+            // Assert
+            Assert.Equal(expectedNumerator, result.Numerator);
+            Assert.Equal(10, result.Denominator);
+        }
+
+        [Fact]
+        public void AddsFractionsWithoutCommonDenominators_Test()
+        {
+            // Arrange
+            var lhs = new Fraction(1, 2);
+            var rhs = new Fraction(2, 3);
+            var expectedNumerator = 7;
+            var expectedDenominator = 6;
+
+            // Act
+            var result = lhs + rhs;
+
+            // Assert
+            Assert.Equal(expectedNumerator, result.Numerator);
+            Assert.Equal(expectedDenominator, result.Denominator);
+        }
+
+        [Fact]
+        public void SubtractsFractionsWithCommonDenominators_Test()
+        {
+            // Arrange
+            var lhs = new Fraction(5, 10);
+            var rhs = new Fraction(2, 10);
+            var expectedNumerator = 7;
+            var expectedDenominator = 10;
+
+            // Act
+            var result = lhs + rhs;
+
+            // Assert
+            Assert.Equal(expectedNumerator, result.Numerator);
+            Assert.Equal(expectedDenominator, result.Denominator);
+        }
+
+        [Fact]
+        public void SubtractsFractionsWithoutCommonDenominators_Test()
+        {
+            // Arrange
+            var lhs = new Fraction(1, 2);
+            var rhs = new Fraction(2, 3);
+            var expectedNumerator = -1;
+            var expectedDenominator = 6;
+
+            // Act
+            var result = lhs - rhs;
+
+            // Assert
+            Assert.Equal(expectedNumerator, result.Numerator);
+            Assert.Equal(expectedDenominator, result.Denominator);
+        }
+
+        [Fact]
+        public void FindsCommonDenominatorBetweenFractions_Test()
+        {
+            // Arrange
+            var lhs = new Fraction(1, 2);
+            var rhs = new Fraction(2, 3);
+            var expectedDenominator = 6;
+
+            // Act
+            int result = Fraction.FindCommonDenominator(lhs, rhs);
+
+            // Assert
+            Assert.Equal(expectedDenominator, result);
+        }
+
         private int SumOfWholeNumbers(int index)
         {
             return index;
--- csharp/DailyChallengesLib/Math/Fraction.cs
+++ csharp/DailyChallengesLib/Math/Fraction.cs
@@ -21,6 +21,82 @@ namespace DailyChallengesLib.Math
         }
 
         /// <summary>
+        /// Unary + operator for a fraction.
+        /// </summary>
+        /// <param name="a">The fraction on the rhs.</param>
+        /// <returns>Original fraction.</returns>
+        public static Fraction operator +(Fraction a)
+        {
+            return a;
+        }
+
+        /// <summary>
+        /// Unary - operator for a fraction.
+        /// </summary>
+        /// <param name="a">The fraction on the rhs.</param>
+        /// <returns>OFraction negated.</returns>
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Numerator, a.Denominator);
+        }
+
+        /// <summary>
+        /// Adds two fractions together.
+        /// </summary>
+        /// <param name="a">left side of the operation.</param>
+        /// <param name="b">right side of the operation.</param>
+        /// <returns>A new fraction, unsimplified, of a+b.</returns>
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            Fraction sum;
+            // we don't have a common denominator
+            // get a common denominator by multplying a * b denominator and b *  a denominator
+            if(a.Denominator != b.Denominator)
+            {
+                var leftNumerator = a.Numerator * b.Denominator;
+                var leftDenominator = a.Denominator * b.Denominator;
+                var rightNumerator = b.Numerator * a.Denominator;
+                sum = AddFraction(leftNumerator, rightNumerator, leftDenominator);
+            }
+            else
+            {
+                sum = AddFraction(a.Numerator, b.Numerator, a.Denominator);
+            }
+
+            return sum;
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            Fraction difference;
+            // we don't have a common denominator
+            // get a common denominator by multplying a * b denominator and b *  a denominator
+            if (a.Denominator != b.Denominator)
+            {
+                var leftNumerator = a.Numerator * b.Denominator;
+                var commonDenomiator = FindCommonDenominator(a, b);
+                var rightNumerator = b.Numerator * a.Denominator;
+                difference = SubtractFraction(leftNumerator, rightNumerator, commonDenomiator);
+            }
+            else
+            {
+                difference = SubtractFraction(a.Numerator, b.Numerator, a.Denominator);
+            }
+
+            return difference;
+        }
+        /// <summary>
+        /// Returns a common denominator between two fractions.
+        /// </summary>
+        /// <param name="a">left hand fraction.</param>
+        /// <param name="b">right hand fraction.</param>
+        /// <returns>Common denomiator between fraction a and b.</returns>
+        public static int FindCommonDenominator(Fraction a, Fraction b)
+        {
+            return a.Denominator * b.Denominator;
+        }
+
+        /// <summary>
         /// The numerator of the fraction.
         /// </summary>
         public int Numerator { get; }
@@ -34,5 +110,25 @@ namespace DailyChallengesLib.Math
         /// The approximated value of a fraction as a decimal.
         /// </summary>
         public double ApproximatedValue => Numerator / Denominator;
+
+        /// <summary>
+        /// The string representation of a fraction.
+        /// Prints out "Numerator / Denominator"
+        /// </summary>
+        /// <returns>string representation of the fraction.</returns>
+        public override string ToString()
+        {
+            return $"{Numerator} / {Denominator}";
+        }
+
+        private static Fraction AddFraction(int aNumerator, int bNumerator, int commonDenominator)
+        {
+            return new Fraction(aNumerator + bNumerator, commonDenominator);
+        }
+
+        private static Fraction SubtractFraction(int aNumerator, int bNumerator, int commonDenominator)
+        {
+            return new Fraction(aNumerator - bNumerator, commonDenominator);
+        }
     }
 }
